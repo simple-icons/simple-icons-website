c40026a965eb1b2b4f47307a226a337f61a9f544
Eric Cornelissen <ericornelissen@gmail.com>
2021-01-26 14:06:48 +0100

    Request feedback in temporary banner
    Add a link to request feedback through the GitHub discussions thread in
    the temporary banner.
    
    Also do some renaming related to the banner, making clear that it is
    meant to be temporary in the source code.

--- public/scripts/oldversion.js
+++ public/scripts/oldversion.js
@@ -1,13 +1,13 @@
 import { hideElement } from './dom-utils.js';
-import { STORAGE_HIDE_INFO_BANNER } from './storage.js';
+import { STORAGE_HIDE_TEMP_BANNER } from './storage.js';
 
 export default function initCopyButtons(document, storage) {
-  const $infoBanner = document.querySelector('.info-oldversion');
-  const $hideInfoButton = document.getElementById('hide-oldversion');
+  const $infoBanner = document.querySelector('.banner-feedback');
+  const $hideInfoButton = document.getElementById('hide-feedback-banner');
 
   $hideInfoButton.disabled = false;
 
-  const storedHideInfoValue = storage.getItem(STORAGE_HIDE_INFO_BANNER);
+  const storedHideInfoValue = storage.getItem(STORAGE_HIDE_TEMP_BANNER);
   if (storedHideInfoValue) {
     hideElement($infoBanner);
   }
@@ -15,6 +15,6 @@ export default function initCopyButtons(document, storage) {
   $hideInfoButton.addEventListener('click', (event) => {
     event.preventDefault();
     hideElement($infoBanner);
-    storage.setItem(STORAGE_HIDE_INFO_BANNER, true);
+    storage.setItem(STORAGE_HIDE_TEMP_BANNER, true);
   });
 }
