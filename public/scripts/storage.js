5f636612912c2cc0d2202514cf279615014239e3
Eric Cornelissen <ericornelissen@gmail.com>
2021-02-02 23:20:27 +0100

    Add temporary banner asking for feedback (#26)
    * Add banner linking back to current simpleicons website
    
    * Request feedback in temporary banner
    
    Add a link to request feedback through the GitHub discussions thread in
    the temporary banner.
    
    * Add option to hide banner permanently
    
    * Add option to hide banner only once

--- public/scripts/storage.js
+++ public/scripts/storage.js
@@ -2,6 +2,7 @@ const PREFIX = 'simple-icons-';
 
 export const STORAGE_KEY_COLOR_SCHEME = PREFIX + 'preferred-color-scheme';
 export const STORAGE_KEY_ORDERING = PREFIX + 'preferred-ordering';
+export const STORAGE_KEY_HIDE_TEMP_BANNER = PREFIX + 'temporary-banner';
 
 const mockStorage = {
   getItem() {},
