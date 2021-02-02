4d74c650cecb1722a85b72c0c1506b0f9f6944d5
Eric Cornelissen <ericornelissen@gmail.com>
2021-02-02 17:52:19 +0100

    Switch from mangler script to WebMangler project

---
+++ webmangler.config.js
@@ -0,0 +1,47 @@
+const BuiltInLanguages = require('webmangler/lib/languages/builtin').default;
+const BuiltInManglers = require('webmangler/lib/manglers/builtin').default;
+
+module.exports = {
+  plugins: [
+    new BuiltInManglers({
+      // CSS class mangling
+      classNamePattern: [
+        // section-related classes
+        '(header|main|footer)[_-]?[a-zA-Z0-9_-]*',
+
+        // grid-related classes
+        'grid-?[a-zA-Z0-9_-]*',
+
+        // control-related classes
+        '(control|search)-?[a-zA-Z0-9_-]*',
+
+        // .order-by-xxx
+        '(order-by)-[a-zA-Z0-9_-]+',
+
+        // .dark-mode and .light-mode
+        '(dark|light)-mode',
+        'contrast-(dark|light)',
+
+        // Miscellaneous
+        '(hidden|no-js|copied|copy-button)',
+      ],
+
+      // CSS variable mangling
+      cssVarNamePattern: '[a-zA-Z0-9\\-]*',
+
+      // Attribute mangling
+      attrNamePattern: 'data-[a-zA-Z0-9\\-]*',
+      keepAttrPrefix: 'data-',
+
+      // ID mangling
+      idNamePattern: [
+        // mostly button IDs
+        'id-[a-zA-Z0-9-]+',
+
+        // IDs for <svg>s
+        '[a-zA-Z0-9-_]+-svg',
+      ],
+    }),
+  ],
+  languages: [new BuiltInLanguages()],
+};
