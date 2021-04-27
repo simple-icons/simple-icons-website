2f086156a1a3f9b626f1f60998b00a5783140276
Eric Cornelissen <ericornelissen@gmail.com>
2021-04-27 21:11:55 +0200

    Un-async copy SVG callback

--- public/scripts/copy.js
+++ public/scripts/copy.js
@@ -24,7 +24,7 @@ export default function initCopyButtons(document, navigator) {
 
   $svgButtons.forEach(($svgButton) => {
     $svgButton.removeAttribute('disabled');
-    $svgButton.addEventListener('click', async (event) => {
+    $svgButton.addEventListener('click', (event) => {
       event.preventDefault();
 
       const $img = $svgButton.querySelector('img');
