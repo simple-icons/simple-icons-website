b91e6141d5555ac1dc1bef5036020f04bb77c001
Eric Cornelissen <ericornelissen@gmail.com>
2021-05-12 18:52:35 +0200

    Update card design (#56)

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -34,7 +34,7 @@ describe('Copy', () => {
     }
 
     document.querySelectorAll.mockImplementation((query) => {
-      if (query === '.grid-item__color') {
+      if (query === '.copy-color') {
         return $colorButtons;
       }
 
@@ -79,7 +79,7 @@ describe('Copy', () => {
     }
 
     document.querySelectorAll.mockImplementation((query) => {
-      if (query === '.grid-item__preview') {
+      if (query === '.copy-svg') {
         return $svgButtons;
       }
 
