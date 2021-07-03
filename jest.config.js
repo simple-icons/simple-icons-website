2abaf97ba620f36700a9e955b497ff79801bb1cb
Eric Cornelissen <ericornelissen@gmail.com>
2021-07-03 19:20:28 +0200

    Rename integration tests to end-to-end tests
    I think both are acceptable, but as the tests are run in a real browser
    I feel like these tests are more accurately called E2E, even if there's
    no backend/database.

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 const TEST_ENV_ALL = 'all';
-const TEST_ENV_INTEGRATION = 'integration';
+const TEST_ENV_E2E = 'e2e';
 const TEST_ENV_UNIT = 'unit';
 
-const TEST_ENV_OPTIONS = [TEST_ENV_ALL, TEST_ENV_INTEGRATION, TEST_ENV_UNIT];
+const TEST_ENV_OPTIONS = [TEST_ENV_ALL, TEST_ENV_E2E, TEST_ENV_UNIT];
 if (!TEST_ENV_OPTIONS.includes(process.env.TEST_ENV)) {
   console.info(`[ERROR] please set TEST_ENV to one of [${TEST_ENV_OPTIONS}]`);
   process.exit(1);
@@ -28,38 +28,38 @@ module.exports = {
 };
 
 function buildWebsite(env) {
-  const buildWebsiteEnvs = [TEST_ENV_ALL, TEST_ENV_INTEGRATION];
+  const buildWebsiteEnvs = [TEST_ENV_ALL, TEST_ENV_E2E];
   if (buildWebsiteEnvs.includes(env)) {
-    console.info('building website for integration tests...');
+    console.info('building website for end-to-end tests...');
     execSync('npm run clean');
     execSync('npm run build');
   }
 }
 
 function getBail(env) {
-  const bailEarlyEnvs = [TEST_ENV_ALL, TEST_ENV_INTEGRATION];
+  const bailEarlyEnvs = [TEST_ENV_ALL, TEST_ENV_E2E];
   return bailEarlyEnvs.includes(env) ? 1 : 0;
 }
 
 function getIgnorePattern(env) {
   const ignorePaths = [];
 
-  const ignoreIntegrationEnvs = [TEST_ENV_UNIT];
-  if (ignoreIntegrationEnvs.includes(env)) {
-    ignorePaths.push('integration.test.js');
+  const ignoreE2eEnvs = [TEST_ENV_UNIT];
+  if (ignoreE2eEnvs.includes(env)) {
+    ignorePaths.push('e2e.test.js');
   }
 
   return ignorePaths;
 }
 
 function getPreset(env) {
-  const jestPuppeteerEnvs = [TEST_ENV_ALL, TEST_ENV_INTEGRATION];
+  const jestPuppeteerEnvs = [TEST_ENV_ALL, TEST_ENV_E2E];
   return jestPuppeteerEnvs.includes(env) ? 'jest-puppeteer' : undefined;
 }
 
 function getTestMatch(env) {
-  if (env === TEST_ENV_INTEGRATION) {
-    return ['**/tests/integration.test.js'];
+  if (env === TEST_ENV_E2E) {
+    return ['**/tests/e2e.test.js'];
   }
 
   return ['**/tests/*.test.js'];
