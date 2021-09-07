b841f2930a295a7f3bf97df063399432015f4d07
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2021-09-07 12:12:02 -0500

    sortByColors import refactoring for testing

--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -1,4 +1,4 @@
-const { sortByColors } = require('../scripts/color-sorting.js');
+const sortByColors = require('../scripts/color-sorting.js');
 
 const fs = require('fs');
 const path = require('path');
