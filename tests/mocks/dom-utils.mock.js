e72bb801a66678352f19092f91b4a82a362e331e
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2021-10-23 15:11:29 -0500

    added unit testing for third party extensions

--- tests/mocks/dom-utils.mock.js
+++ tests/mocks/dom-utils.mock.js
@@ -1,10 +1,12 @@
 export const domUtils = {
   hideElement: jest.fn().mockName('domUtils.hideElement'),
   showElement: jest.fn().mockName('domUtils.showElement'),
+  toggleVisibleElement: jest.fn().mockName('domUtils.toggleVisibleElement'),
 
   // Utility to quickly clear the entire dom-utils mock.
   __resetAllMocks: function () {
     this.hideElement.mockReset();
     this.showElement.mockReset();
+    this.toggleVisibleElement.mockReset();
   },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const simpleIcons = require('simple-icons');
-
+const fetch = require('sync-fetch');
 const { normalizeSearchTerm } = require('./public/scripts/utils.js');
 const sortByColors = require('./scripts/color-sorting.js');
 
@@ -16,6 +16,27 @@ const NODE_MODULES = path.resolve(__dirname, 'node_modules');
 const OUT_DIR = path.resolve(__dirname, '_site');
 const ROOT_DIR = path.resolve(__dirname, 'public');
 
+function parseExtensions() {
+  const response = fetch(
+    'https://raw.githubusercontent.com/simple-icons/simple-icons/develop/README.md',
+  );
+  const body = response.text();
+  const extRegExp = /## Third-Party Extensions\n\n(.*)\[slug\]/gs;
+  return Array.from(body.match(extRegExp))[0]
+    .split('\n')
+    .filter((line) => line.startsWith('| ['))
+    .map((line) => {
+      const module = line.split(' | ')[0];
+      const author = line.split(' | ')[1];
+      return {
+        nameModule: module.match(/\[(.*?)\]/gs)[0].slice(1, -1),
+        urlModule: module.match(/\((.*?)\)/gs)[0].slice(1, -1),
+        nameAuthor: author.match(/\[(.*?)\]/gs)[0].slice(1, -1),
+        urlAuthor: author.match(/\((.*?)\)/gs)[0].slice(1, -1),
+      };
+    });
+}
+
 function simplifyHexIfPossible(hex) {
   if (hex[0] === hex[1] && hex[2] === hex[3] && hex[4] == hex[5]) {
     return `${hex[0]}${hex[2]}${hex[4]}`;
@@ -24,6 +45,7 @@ function simplifyHexIfPossible(hex) {
   return hex;
 }
 
+let extensions = parseExtensions();
 let displayIcons = icons;
 if (process.env.TEST_ENV) {
   // Use fewer icons when building for a test run. This significantly speeds up
@@ -83,6 +105,7 @@ module.exports = {
       inject: true,
       template: path.resolve(ROOT_DIR, 'index.pug'),
       templateParameters: {
+        extensions: extensions,
         icons: displayIcons.map((icon, iconIndex) => {
           const luminance = getRelativeLuminance(`#${icon.hex}`);
           return {
