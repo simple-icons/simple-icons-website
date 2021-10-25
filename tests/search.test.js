0b6ad9cc202c5b08c7af858d3e208b382c7aac18
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2021-10-25 15:14:59 -0500

    Button design (#123)
    * joined button groups, rounded the borders only for the first and last button on each group
    
    * Update public/stylesheet.css
    
    Co-authored-by: Eric Cornelissen <ericornelissen@gmail.com>
    
    * added toggle Function to domUtils, worked on suggestions by Eric for the corresponding PR
    
    * added toggle Function to domUtils, worked on suggestions by Eric for the corresponding PR
    
    * added toggle Function to domUtils, worked on suggestions by Eric for the corresponding PR
    
    * .
    
    * updated dom-utils test
    
    * removed margins from buttons, reworked the border-left config in the first__button
    
    * fixed toggle call in search.js
    
    * fixed toggle call in search.js
    
    * fixed toggle call in search.js
    
    * added tests for dom-utils addClass and removeClass
    
    Co-authored-by: Eric Cornelissen <ericornelissen@gmail.com>

--- tests/search.test.js
+++ tests/search.test.js
@@ -17,6 +17,7 @@ describe('Search', () => {
   let $searchInput;
   let $searchClear;
   let $orderByRelevance;
+  let $orderByColor;
 
   beforeAll(() => {
     jest.useFakeTimers();
@@ -42,6 +43,7 @@ describe('Search', () => {
     });
 
     $orderByRelevance = newElementMock('#order-relevance');
+    $orderByColor = newElementMock('#order-color');
 
     document.getElementById.mockImplementation((query) => {
       switch (query) {
@@ -51,6 +53,8 @@ describe('Search', () => {
           return $searchClear;
         case 'order-relevance':
           return $orderByRelevance;
+        case 'order-color':
+          return $orderByColor;
         default:
           return newElementMock(query);
       }
@@ -95,6 +99,14 @@ describe('Search', () => {
 
       expect(domUtils.showElement).toHaveBeenCalledWith($searchClear);
       expect(domUtils.showElement).toHaveBeenCalledWith($orderByRelevance);
+      expect(domUtils.addClass).toHaveBeenCalledWith(
+        $orderByRelevance,
+        'last__button',
+      );
+      expect(domUtils.removeClass).toHaveBeenCalledWith(
+        $orderByColor,
+        'last__button',
+      );
 
       done();
     });
@@ -124,7 +136,14 @@ describe('Search', () => {
 
       expect(domUtils.hideElement).toHaveBeenCalledWith($searchClear);
       expect(domUtils.hideElement).toHaveBeenCalledWith($orderByRelevance);
-
+      expect(domUtils.removeClass).toHaveBeenCalledWith(
+        $orderByRelevance,
+        'last__button',
+      );
+      expect(domUtils.addClass).toHaveBeenCalledWith(
+        $orderByColor,
+        'last__button',
+      );
       done();
     });
 
@@ -153,7 +172,14 @@ describe('Search', () => {
 
       expect(domUtils.hideElement).toHaveBeenCalledWith($searchClear);
       expect(domUtils.hideElement).toHaveBeenCalledWith($orderByRelevance);
-
+      expect(domUtils.removeClass).toHaveBeenCalledWith(
+        $orderByRelevance,
+        'last__button',
+      );
+      expect(domUtils.addClass).toHaveBeenCalledWith(
+        $orderByColor,
+        'last__button',
+      );
       done();
     });
   });
@@ -183,6 +209,8 @@ describe('Search', () => {
 
       expect($searchInput.value).toEqual(expected);
       expect(domUtils.showElement).toHaveBeenCalled();
+      expect(domUtils.addClass).toHaveBeenCalled();
+      expect(domUtils.removeClass).toHaveBeenCalled();
       expect(history.replaceState).toHaveBeenCalled();
       expect(ordering.selectOrdering).toHaveBeenCalled();
     });
