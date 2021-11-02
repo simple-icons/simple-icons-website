f2db362a62e1d9ee50ca5a8ff5ca154abf39b5fd
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2021-11-01 23:03:17 -0600

    changed modal border to 0.13rem, added a click handler to body to close modal

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,13 +1,18 @@
 export default async function initModal(document, domUtils) {
   const $modalTrigger = document.querySelector('.popup-trigger');
   const $popupModal = document.querySelector('.popup_modal');
-  $modalTrigger.addEventListener('click', () => {
+  $modalTrigger.addEventListener('click', (e) => {
     domUtils.toggleVisibleElement($popupModal);
+    e.stopPropagation();
   });
 
   document.addEventListener('keyup', (e) => {
     if (e.key === 'Escape') {
       domUtils.hideElement($popupModal);
     }
+  });
+
+  document.body.addEventListener('click', (e) => {
+    domUtils.hideElement($popupModal);
   });
 }
