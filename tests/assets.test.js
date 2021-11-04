2ed1312630f3f8e2fcdd2f6c84f7fcf41c2ed70a
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-11-04 14:38:36 +0100

    Test embedded icons optimization with simple-icons SVGO config

---
+++ tests/assets.test.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const pugLex = require('pug-lexer');
+const { optimize } = require('svgo');
+const svgoConfig = require('simple-icons/svgo.config.js');
+
+const extractContextsSvgPaths = (tokens) => {
+  return tokens
+    .filter((token) => {
+      return (
+        token.type === 'attribute' &&
+        ((token.name === 'd' && !token.val.includes('path')) ||
+          token.name === 'title')
+      );
+    })
+    .map((token, i, tokens) => {
+      const prevToken = i === 0 ? { type: null, name: null } : tokens[i - 1];
+      if (
+        token.type === 'attribute' &&
+        token.name === 'd' &&
+        prevToken.type === 'attribute' &&
+        prevToken.name === 'title'
+      ) {
+        return [
+          prevToken.val.substring(1, prevToken.val.length - 1), // title
+          token.val.substring(1, token.val.length - 1), // path
+        ];
+      }
+    })
+    .filter(Boolean);
+};
+
+describe('Embedded assets optimization', () => {
+  const indexFilePath = path.join('public', 'index.pug');
+  const indexFileContent = fs.readFileSync(indexFilePath).toString();
+  const contextsSvgPaths = extractContextsSvgPaths(pugLex(indexFileContent));
+
+  it.each(contextsSvgPaths)(
+    '%p icon path is optimized with SVGO',
+    (title, path) => {
+      const svg = `<svg><path d="${path}"/></svg>`;
+      expect(svg).toEqual(optimize(svg).data);
+    },
+  );
+});
