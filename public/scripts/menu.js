6dbe54675d652cc3dbb0308d8007391359b33922
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2022-01-23 12:42:07 -0600

    replaced hidden-768 for hidden-gt-768px at Mondeja's request

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -2,6 +2,6 @@ export default async function initMenu(document, domUtils) {
   document
     .querySelector('#menu-Toggle svg')
     .addEventListener('click', (e) =>
-      domUtils.toggleClass(document.querySelector('#menu'), 'hidden-768'),
+      domUtils.toggleClass(document.querySelector('#menu'), 'hidden-gt-768px'),
     );
 }
