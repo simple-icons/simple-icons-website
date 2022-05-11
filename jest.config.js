5b45296c312fd6cdd715c7aca3c69c33f3e06ef1
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-05-11 21:51:48 +0200

    Simplify Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,69 +1,35 @@
-import { execSync } from 'child_process';
-import path from 'path';
+import { execSync } from 'node:child_process';
+import path from 'node:path';
 import { getDirnameFromImportMeta } from './si-utils.js';
 
 const __dirname = getDirnameFromImportMeta(import.meta.url);
 
-const TEST_ENV_ALL = 'all';
-const TEST_ENV_E2E = 'e2e';
-const TEST_ENV_UNIT = 'unit';
+const TEST_ENV_OPTIONS = ['all', 'e2e', 'unit'];
+const [_, TEST_ENV_E2E, TEST_ENV_UNIT] = TEST_ENV_OPTIONS;
+const env = process.env.TEST_ENV;
 
-const TEST_ENV_OPTIONS = [TEST_ENV_ALL, TEST_ENV_E2E, TEST_ENV_UNIT];
-if (!TEST_ENV_OPTIONS.includes(process.env.TEST_ENV)) {
-  console.info(`[ERROR] please set TEST_ENV to one of [${TEST_ENV_OPTIONS}]`);
+if (!TEST_ENV_OPTIONS.includes(env)) {
+  console.error(
+    `Please set TEST_ENV environment variable to one of [${TEST_ENV_OPTIONS}]`,
+  );
   process.exit(1);
 }
 
-const buildWebsite = (env) => {
-  const buildWebsiteEnvs = [TEST_ENV_ALL, TEST_ENV_E2E];
-  if (buildWebsiteEnvs.includes(env)) {
-    console.info('building website for end-to-end tests...');
-    execSync('npm run clean');
-    execSync('npm run build');
-  }
-};
-
-const getBail = (env) => {
-  const bailEarlyEnvs = [TEST_ENV_ALL, TEST_ENV_E2E];
-  return bailEarlyEnvs.includes(env) ? 1 : 0;
-};
-
-const getIgnorePattern = (env) => {
-  const ignorePaths = [];
-
-  const ignoreE2eEnvs = [TEST_ENV_UNIT];
-  if (ignoreE2eEnvs.includes(env)) {
-    ignorePaths.push('e2e.test.js');
-  }
-
-  return ignorePaths;
-};
-
-const getPreset = (env) => {
-  const jestPuppeteerEnvs = [TEST_ENV_ALL, TEST_ENV_E2E];
-  return jestPuppeteerEnvs.includes(env) ? 'jest-puppeteer' : undefined;
-};
-
-const getTestMatch = (env) => {
-  if (env === TEST_ENV_E2E) {
-    return ['**/tests/e2e.test.js'];
-  }
-
-  return ['**/tests/*.test.js'];
-};
-
-buildWebsite(process.env.TEST_ENV);
+if (env !== TEST_ENV_UNIT) {
+  console.info('Building website for end-to-end tests...');
+  execSync('run-s clean build');
+}
 
 export default {
-  bail: getBail(process.env.TEST_ENV),
+  bail: env === TEST_ENV_UNIT ? 0 : 1,
   cacheDirectory: './.cache/jest',
-  preset: getPreset(process.env.TEST_ENV),
+  preset: env === TEST_ENV_UNIT ? undefined : 'jest-puppeteer',
   globals: {
     ARTIFACTS_DIR: path.resolve(__dirname, 'tests/_artifacts'),
   },
-  testMatch: [...getTestMatch(process.env.TEST_ENV)],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    ...getIgnorePattern(process.env.TEST_ENV),
+  testMatch: [
+    `**/tests/${
+      env === TEST_ENV_UNIT ? '!(e2e)' : env === TEST_ENV_E2E ? 'e2e' : '*'
+    }.test.js`,
   ],
 };
