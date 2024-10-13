1583ca7a1cf6305ea9cab879d42c2b947b285522
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-10-13 05:52:47 +0200

    No sandboxed puppeteer launch (#343)

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -3,6 +3,7 @@ const process = require('node:process');
 module.exports = {
   launch: {
     headless: process.env.TEST_HEADLESS !== 'false',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
   server: {
     command: 'npm run serve',
